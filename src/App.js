5bc0184bc0f537b200600b63e6530202c454bb73
install install <install@gtc.eif.local>
2023-05-19 16:15:22 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
-import ToDo from "./components/ToDo";
+import { useState, useMemo, useCallback, useEffect } from "react";
+
 function App() {
+  const [name, setName] = useState('test');
+  //const [surname, setSurname] = useState('');
+  const [count, setCount] = useState(0);
+
+  // const user = useMemo(() => {
+  //   console.log('ok');
+  //   return {
+  //     name: name,
+  //     surname: surname
+  //   }
+  // }, [name, surname]);
+
+  const logName = useCallback(() => {
+    console.log(name);
+
+  }, [name]);
+
+  useEffect(() => {
+    logName(name);
+
+  }, [logName, name])
+
   return (
     <>
-    <ToDo />
+      {/* <button onClick={() => setCount(count + 1)}>{count}</button>
+     <input type="text" value={name} onChange={e=>setName(e.target.value)}></input>
+     <input type="text" value={surname} onChange={e=>setSurname(e.target.value)}></input>
+     <p>{JSON.stringify(user)}</p> */}
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+
     </>
   );
 }
